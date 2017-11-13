98c486a480d893bb02cbfc621453ebc52481de21
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2017-11-13 06:17:54 +0000

    Add files via upload

---
+++ SeriesFinder.website/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace SeriesFinder.website
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>().UseKestrel(options =>
+			{
+            //options.Listen(IPAddress.Loopback, 5001);
+			options.Listen(IPAddress.Any, 5001);
+			}).Build();
+    }
+}
